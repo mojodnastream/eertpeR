064b18ad454b89806e8873e044da03fe16ad27b0
mojodnastream <gnothom@gmail.com>
2016-08-02 11:04:39 -0500

    First Commit - Baseline

---
+++ eertpeR/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  eertpeR
+//
+//  Created by Gary Nothom on 8/2/16.
+//  Copyright © 2016 Mojo Services. All rights reserved.
+//
+
+import Foundation
+
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
---
+++ eertpeR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eertpeR
+//
+//  Created by Gary Nothom on 8/2/16.
+//  Copyright © 2016 Mojo Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
