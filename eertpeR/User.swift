ac49d932cb3dcb6bdfeadc99d2214bbc5767b7ae
mojodnastream <gnothom@gmail.com>
2016-08-02 12:57:02 -0700

    Started scaffolding out class structure

---
+++ eertpeR/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  eertpeR
+//
+//  Created by Gary Nothom on 8/2/16.
+//  Copyright © 2016 Mojo Services. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
