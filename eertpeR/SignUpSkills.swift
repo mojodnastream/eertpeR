b3d5a286e8fa2aa4b56763183e051362c70779df
mojodnastream <gnothom@gmail.com>
2017-12-21 14:14:20 -0800

    Changed view from VC to a NVC, which required changing quite a bit of the underlying code.

--- eertpeR/SignUpSkills.swift
+++ eertpeR/SignUpSkills.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
-//import Parse
+import Firebase
 
-class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdating, UITableViewDataSource {
+class signUpSkills: UITableViewController, UISearchResultsUpdating {
+    
     
     var resultSearchController = UISearchController(searchResultsController: nil)
+    var refUserSkill = FIRDatabase.database().reference(withPath: "Profile");
     
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet var doneBtnStyle: UIButton!
-    @IBAction func doneBtn(_ sender: UIButton) {
+    @IBAction func btnComplete(_ sender: UIBarButtonItem) {
         finalizeSignUp()
-        //add user added skills (not found in lookup table) to the skills lookup
-        //not sure i want to do this upon sign up though, let's think it through
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+
+    override func viewDidLoad() {
+        
+        self.setNeedsStatusBarAppearanceUpdate()
+
+        //style the button
+        loadSkills()
+        
+        //clear array in case the app crashed in the middle of sign up
+        arrSkillsForUser.removeAll()
+        arrSkills.removeAll()
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red:0.145, green:0.075, blue:0.384, alpha:1.00)
+        navigationController?.navigationBar.isTranslucent = false
+       
+        //set up the search box
+        resultSearchController.searchResultsUpdater = self
+        resultSearchController.dimsBackgroundDuringPresentation = false
+        resultSearchController.searchBar.sizeToFit()
+        resultSearchController.searchBar.tintColor = UIColor.white
+        resultSearchController.searchBar.barTintColor = UIColor(red:0.145, green:0.075, blue:0.384, alpha:1.00)
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.searchBar.placeholder = "Add a Few Skills"
+        resultSearchController.searchBar.showsCancelButton = false
+        resultSearchController.searchBar.setValue("Done", forKey: "_cancelButtonText")
+        resultSearchController.definesPresentationContext = true
+        tableView.tableHeaderView = resultSearchController.searchBar
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        resultSearchController.searchBar.isHidden = false
     }
     
     func finalizeSignUp() {
@@ -26,29 +58,34 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
             for skill in arrSkillsForUser {
                 addSkillToProfile(skill: skill)
             }
+            self.performSegue(withIdentifier: "skillsToProfile", sender: self)
+            
         }
         else {
             print("you must choose at least one skill")
         }
-        self.performSegue(withIdentifier: "skillsToProfile", sender: self)
+        
     }
     
     func addSkillToProfile(skill: String) {
-//        let userSkills = PFObject(className: "UserSkills")
-//        userSkills["userSkill"] = skill.trimmingCharacters(in: NSCharacterSet.whitespaces)
-//        userSkills["userID"] = PFUser.current()?.objectId
-//        userSkills.saveInBackground { (success, error) -> Void in
-//
-//            if success {
-//                print("user skill \(skill) has been saved.")
-//            } else {
-//                if error != nil {
-//                    //print ("oops \(error ?? "an error happened")")
-//                } else {
-//                    print ("No Errors")
-//                }
-//            }
-//        }
+        
+        let theSkill = refUserSkill.child("Skills")
+        
+        let key = theSkill.childByAutoId().key
+        
+        //creating artist with the given values
+        let skillToAdd = ["id": key,
+                       "skill": skill.trimmingCharacters(in: NSCharacterSet.whitespaces)
+        ]
+        
+        //adding the artist inside the generated unique key
+        theSkill.child(key).setValue(skillToAdd, withCompletionBlock: { (error, snapshot) in
+            if error != nil {
+                print(error?.localizedDescription ?? "No error description available")
+            } else {
+                print("added skill \(skill) to profile")
+            }
+        })
     }
     
     func loadSkills() {
@@ -73,7 +110,7 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
 //        }
     }
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
         
         if arrSkillsForUser.contains((cell?.textLabel?.text)!) {
@@ -86,7 +123,7 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("selected row \(indexPath.row)")
         
         let cell = tableView.cellForRow(at: indexPath)
@@ -110,11 +147,11 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
         
     }
    
-    func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if(resultSearchController.isActive) {
             if arrSkillsSearchResults.count > 0 {
@@ -134,7 +171,7 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
         }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "SkillCell", for: indexPath as IndexPath) as UITableViewCell
         
@@ -160,7 +197,7 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
             }
         }
         else {
-            //print(arrSkillsForUser.count)
+            print(arrSkillsForUser.count)
             cell.textLabel?.font = UIFont(name:"HelveticaNeue", size:20)
             if arrSkillsForUser.count > 0 {
                 arrSkillsForUser.sort()
@@ -194,34 +231,5 @@ class signUpSkills: UIViewController, UITableViewDelegate, UISearchResultsUpdati
             })
             tableView.reloadData()
         }
-    }
-    
-    override func viewDidLoad() {
-        //style the button
-        loadSkills()
-        
-        //clear array in case the app crashed in the middle of sign up
-        arrSkillsForUser.removeAll()
-        arrSkills.removeAll()
-
-        //establish data delegates from the tableview
-        tableView.delegate = self;
-        tableView.dataSource = self;
-        
-        //style the Done Buttona
-        doneBtnStyle.layer.cornerRadius = 5
-        doneBtnStyle.layer.borderWidth = 1
-        doneBtnStyle.layer.borderColor = UIColor.purple.cgColor
-        
-        //set up the search box
-        resultSearchController.searchResultsUpdater = self
-        resultSearchController.dimsBackgroundDuringPresentation = false
-        resultSearchController.searchBar.sizeToFit()
-        resultSearchController.hidesNavigationBarDuringPresentation = false
-        resultSearchController.searchBar.placeholder = "Search Skills"
-        resultSearchController.searchBar.showsCancelButton = false
-        resultSearchController.searchBar.setValue("Done", forKey: "_cancelButtonText")
-        tableView.contentInset = UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
-        tableView.tableHeaderView = resultSearchController.searchBar
     }
 }
