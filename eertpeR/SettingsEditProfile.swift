a99409c862bf4e999e09e21b1ca7eab4c88a3dd4
mojodnastream <gnothom@gmail.com>
2018-01-02 14:41:25 -0800

    Added screen for profile edit, and scaffolding for the view controller.

---
+++ eertpeR/SettingsEditProfile.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsEditProfile.swift
+//  eertpeR
+//
+//  Created by Frosty on 1/2/18.
+//  Copyright © 2018 Mojo Services. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class settingsEditProfile: UIViewController {
+    
+    
+    @IBOutlet weak var txtDisplayName: UITextField!
+    @IBOutlet weak var txtEditTitle: UITextField!
+    @IBOutlet weak var txtEditCompanyName: UITextField!
+    @IBOutlet weak var txtEditLoc: UITextField!
+    @IBAction func btnSaveEdits(_ sender: Any) {
+    }
+    
+}
