aea0a01da61dbb0e15e6cf69897b91d0108fd5f5
mojodnastream <gnothom@gmail.com>
2018-01-25 14:52:39 -0800

    Code cleanup

--- eertpeR/User.swift
+++ eertpeR/User.swift
@@ -51,24 +51,6 @@ class getUsers {
                         }
                     }
                 }
-                
-//                for child in snapshot.children {
-//                    let dict = child as! [String: Any]
-//                    let name = dict["company"] as! String
-//                    let id = dict["id"] as! String
-//
-//                    arrSearchResults.append("Member~\(name)*\(id)")
-//                    print("name/key: \(name) : \(id)")
-//                }
-                
-//                let values = snapshot.value as! [String: AnyObject]
-//                //            constUserCompany = values["company"] as! String
-//
-//                var userid = values["company"] as! String
-//                var name = values["name"] as! String
-//                userid = "" //(object["userID"] as? String!)!
-//                name =  "" //"\(firstname) \(lastname)"
-                //arrSearchResults.append("Member~\(name)*\(userid)")
             })
         }
 }
