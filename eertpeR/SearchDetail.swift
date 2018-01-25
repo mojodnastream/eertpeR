171153606ac8f569624fddcfda2f2b7ef6336184
mojodnastream <gnothom@gmail.com>
2018-01-25 15:13:25 -0800

    Added methods to pull skills and badges associate with the user detail user.

--- eertpeR/SearchDetail.swift
+++ eertpeR/SearchDetail.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import Parse
+import Firebase
 
 class SearchDetail: UIViewController {
     var passUserID:String!
@@ -19,6 +19,8 @@ class SearchDetail: UIViewController {
     @IBOutlet weak var userTitle: UILabel!
     @IBOutlet var userProfileIMG: UIImageView!
     @IBOutlet weak var entityName: UILabel!
+    
+    let userProfileDetailRef = Database.database().reference(withPath: "Profiles")
     
     func setUserVars() {
         self.title = passUserName
@@ -37,6 +39,61 @@ class SearchDetail: UIViewController {
             }
             
         }
+    }
+    
+    func loadBadges() {
+        userProfileDetailRef.child(passUserID).child("Badges").observe(.value, with: {
+            snapshot in
+            guard snapshot.exists() else {
+                //print("no data yet - grab badges from the array is this is the initial sign up load")
+                if arrBadges.count > 0 {
+                    //print("Badges I sugned Up with \(arrBadges.count)")
+                }
+                else {
+                    print("no badges from sign up")
+                }
+                return
+            }
+            
+            for child in (snapshot.children) {
+                
+                let snap = child as! DataSnapshot //each child is a snapshot
+                let dict = snap.value as! [String: String] // the value is a dict
+                
+                //let badgeID = dict["id"]
+                let badge = dict["badge"]
+                arrBadgesProfileUsage.append(badge!)
+            }
+            //let userBadgesCounter = arrBadgesProfileUsage.count
+            //self.circBadges.text = String(userBadgesCounter)
+        })
+    }
+    
+    func loadSkills() {
+        //user = User(uid: userID, email: FIRAuth.auth()?.currentUser?.email)
+        userProfileDetailRef.child(passUserID).child("Skills").observe(.value, with: {
+            snapshot in
+            guard snapshot.exists() else {
+                //print("no data yet - grab skills from the array is this is the initial sign up load")
+                if arrSkillsSignUp.count > 0 {
+                    //print("Skills I sugned Up with \(arrSkillsSignUp.count)")
+                }
+                else {
+                    print("no skills from sign up")
+                }
+                return
+            }
+            
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                for child in snapshots {
+                    let values = child.value as! [String: AnyObject]
+                    let skill = values["skill"] as! String
+                    arrSkillsProfileUsage.append(skill)
+                }
+            }
+            //userSkills = arrSkillsProfileUsage.count
+            //self.circSkills.text = String(userSkills)
+        })
     }
     
     override func viewDidLoad() {
