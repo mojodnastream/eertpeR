178862783c8bfdac9724b63640d45453f817518f
mojodnastream <gnothom@gmail.com>
2018-01-26 13:11:58 -0800

    Added Reachability scaffolding for use later

--- eertpeR/AppDelegate.swift
+++ eertpeR/AppDelegate.swift
@@ -23,6 +23,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
        
     }
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        // Starts monitoring network reachability status changes
+        ReachabilityManager.shared.startMonitoring()
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        // Stops monitoring network reachability status changes
+        ReachabilityManager.shared.stopMonitoring()
+    }
+    
     private func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
 //        if error.code == 3010 {
 //            print("Push notifications are not supported in the iOS Simulator.\n")
