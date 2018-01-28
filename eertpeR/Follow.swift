77dcbb15ea88cbe4a7a87f6530bb6cd252c3fec4
mojodnastream <gnothom@gmail.com>
2018-01-27 16:33:48 -0800

    Added Follow Class to manage followers

---
+++ eertpeR/Follow.swift
@@ -0,0 +1,16 @@
+//
+//  Follow.swift
+//  eertpeR
+//
+//  Created by Frosty on 1/27/18.
+//  Copyright © 2018 Mojo Services. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Follow {
+    var userID: String?
+    var sinceWhen: NSNumber?
+}
+
