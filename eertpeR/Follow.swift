446b176f0afb3501b266e664f624c39888aeadcd
mojodnastream <gnothom@gmail.com>
2018-01-29 14:59:47 -0800

    Added class for Follows

--- eertpeR/Follow.swift
+++ eertpeR/Follow.swift
@@ -14,3 +14,37 @@ class Follow {
     var sinceWhen: NSNumber?
 }
 
+class getFollows {
+    
+    static func loadUserInfo() {
+        arrSearchResults.removeAll()
+        arrUserClassArray.removeAll()
+        let userDBRef = Database.database().reference(withPath: "Profiles")
+        
+        userDBRef.observe(.value, with: {
+            snapshot in
+            guard snapshot.exists() else {
+                print("no user info")
+                return
+            }
+            
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                for child in snapshots {
+                    let id = child.key
+                    if id != userID {
+                        var id = "bobb"
+                        var since = 0
+                        let values = child.value as! [String: AnyObject]
+                        id = values["id"] as! String
+                        since = values["sinceWhen"] as! Int
+                        
+                        let follow = Follow()
+                        follow.userID = id
+                        follow.sinceWhen = since as NSNumber
+                        arrFollowingClassArray.append(follow)
+                    }
+                }
+            }
+        })
+    }
+}
