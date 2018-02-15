77638d86a05301b26ff8b4a06782e7557df31947
mojodnastream <gnothom@gmail.com>
2018-02-15 15:23:53 -0800

    Badge Detail VC

---
+++ eertpeR/SearchDetailBadges.swift
@@ -0,0 +1,15 @@
+//
+//  SearchDetailBadges.swift
+//  eertpeR
+//
+//  Created by Frosty on 2/15/18.
+//  Copyright © 2018 Mojo Services. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class BadgeDetail: UIViewController {
+    var passUserID:String!
+    var passUserName:String!
+}
